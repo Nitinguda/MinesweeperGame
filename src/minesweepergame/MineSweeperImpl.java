6366fa6cb69e5a61dbc4df27a7ef9062df97e00c
SAITEJA <saiteja24nov@hotmail.com>
2017-03-14 15:12:31 -0400

    minesweeper
    This is a game with 8x8 grid where mines are placed randomly and count
    of mines around each block.
    Game is to open al the blocks without stepping on the mines.

---
+++ src/minesweepergame/MineSweeperImpl.java
@@ -0,0 +1,199 @@
+package minesweepergame;
+
+import java.awt.Color;
+import javax.swing.JButton;
+
+public class MineSweeperImpl {
+
+    int[][] grid;
+
+    public MineSweeperImpl() {
+        initiategridMines();
+        formMinesweep();
+    }
+
+    int clicked(int i, int j) {
+        //System.out.println(i+"clicked()"+j);
+        int selected= grid[i][j];
+        return selected;
+        
+    }
+
+    private void initiategridMines() {
+        grid = new int[8][8];
+        for (int k = 0; k < 12; k++) {
+            int i = (int) (Math.random() * (7 - 0) + 0);
+            int j = (int) (Math.random() * (7 - 0) + 0);
+            grid[i][j] = 9;
+        }
+        display();
+    }
+    private void display(){
+        for (int i = 0; i < 8; i++) {
+            for(int j=0;j<8;j++){
+                System.out.print(grid[i][j]+"  ");
+            }
+            System.out.println();
+        }
+         System.out.println();
+    }
+
+    public void formMinesweep() {
+        int sum=0;
+        for (int i = 0; i < 8; i++) {
+            for(int j=0;j<8;j++){
+                if(grid[i][j]!=9){
+                if(i==0 && j==0){
+                    if(grid[i+1][j]==9)
+                        sum+=1;
+                    if(grid[i][j+1]==9)
+                        sum+=1;
+                    if(grid[i+1][j+1]==9)
+                        sum+=1;
+                    grid[i][j]=sum;
+                }
+                else if(i==0 && j==7){
+                    if(grid[i][j-1]==9)
+                        sum+=1;
+                    if(grid[i+1][j]==9)
+                        sum+=1;
+                    if(grid[i+1][j-1]==9)
+                        sum+=1;
+                    grid[i][j]=sum;
+                    
+                }
+                else if(i==7 && j==0){
+                    if(grid[i][j+1]==9)
+                        sum+=1;
+                    if(grid[i-1][j]==9)
+                        sum+=1;
+                    if(grid[i-1][j+1]==9)
+                        sum+=1;
+                    grid[i][j]=sum;
+                }
+                else if(i==7 && j==7){
+                    if(grid[i][j-1]==9)
+                        sum+=1;
+                    if(grid[i-1][j]==9)
+                        sum+=1;
+                    if(grid[i-1][j-1]==9)
+                        sum+=1;
+                    grid[i][j]=sum;
+                }
+                else if(i==0){
+                    if(grid[i][j-1]==9)
+                        sum+=1;
+                    if(grid[i][j+1]==9)
+                        sum+=1;
+                    if(grid[i+1][j-1]==9)
+                        sum+=1;
+                    if(grid[i+1][j+1]==9)
+                        sum+=1;
+                    if(grid[i+1][j]==9)
+                        sum+=1;
+                    grid[i][j]=sum;
+                }
+                else if(j==0){
+                    if(grid[i-1][j]==9)
+                        sum+=1;
+                    if(grid[i+1][j]==9)
+                        sum+=1;
+                    if(grid[i-1][j+1]==9)
+                        sum+=1;
+                    if(grid[i][j+1]==9)
+                        sum+=1;
+                    if(grid[i+1][j+1]==9)
+                        sum+=1;
+                    grid[i][j]=sum;
+                }
+                else if(i==7){
+                    if(grid[i][j-1]==9)
+                        sum+=1;
+                    if(grid[i][j+1]==9)
+                        sum+=1;
+                    if(grid[i-1][j-1]==9)
+                        sum+=1;
+                    if(grid[i-1][j]==9)
+                        sum+=1;
+                    if(grid[i-1][j+1]==9)
+                        sum+=1;
+                    grid[i][j]=sum;
+                }
+                else if(j==7){
+                    if(grid[i-1][j]==9)
+                        sum+=1;
+                    if(grid[i+1][j]==9)
+                        sum+=1;
+                    if(grid[i-1][j-1]==9)
+                        sum+=1;
+                    if(grid[i][j-1]==9)
+                        sum+=1;
+                    if(grid[i+1][j-1]==9)
+                        sum+=1;
+                    grid[i][j]=sum;
+                }
+                else{
+                    if(grid[i-1][j-1]==9)
+                        sum+=1;
+                    if(grid[i-1][j]==9)
+                        sum+=1;
+                    if(grid[i-1][j+1]==9)
+                        sum+=1;
+                    if(grid[i][j-1]==9)
+                        sum+=1;
+                    if(grid[i][j+1]==9)
+                        sum+=1;
+                    if(grid[i+1][j-1]==9)
+                        sum+=1;
+                    if(grid[i+1][j]==9)
+                        sum+=1;
+                    if(grid[i+1][j+1]==9)
+                        sum+=1;
+                    grid[i][j]=sum;
+                }
+                  sum=0;  
+             }
+            }
+        }
+        
+    display();
+    }
+
+
+    void gameOver(JButton[][] list) {
+        for (int i = 0; i < 8; i++) {
+            for(int j=0;j<8;j++){
+                if(grid[i][j]==9){
+                    list[i][j].setText("X");
+                    list[i][j].setBackground(Color.red);
+                }
+                
+                list[i][j].setEnabled(false);
+            }
+        }
+    }
+
+    void openZeros(JButton[][] list,int i,int j) {
+        for (int a = i-1; a<=i+1; a++) {
+            for(int b=j-1;b<=j+1;b++){
+                if(a<0||b<0||a>7||b>7){
+                }
+                else{
+                    int ans=clicked(a,b);
+                    if(ans==0 && list[a][b].isEnabled()==true){
+                        System.out.println("a="+a+"b="+b);
+                        list[a][b].setText(Integer.toString(ans));
+                        list[a][b].setBackground(Color.yellow);
+                        list[a][b].setEnabled(false);
+                        openZeros(list, a, b);
+                    }else{
+                    list[a][b].setText(Integer.toString(ans));
+                    list[a][b].setBackground(Color.yellow);
+                    list[a][b].setEnabled(false);
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ src/minesweepergame/MinesweeperGame.java
@@ -0,0 +1,112 @@
+package minesweepergame;
+
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicBorders;
+import javax.swing.plaf.basic.BasicBorders.ButtonBorder;
+
+public class MinesweeperGame extends JFrame implements ActionListener {
+
+    MineSweeperImpl msi = new MineSweeperImpl();
+    JButton[][] list = new JButton[8][8];
+    JPanel jPanel1 = new JPanel();
+    JPanel jPanel2 = new JPanel();
+    JPanel jPanel3 = new JPanel();
+    Dimension frameD = new Dimension(500, 375);
+    Dimension PanalD = new Dimension(500, 400);
+    public MinesweeperGame() {
+
+    }
+
+    void initComponents() {
+
+        setTitle("MINESWEEPER");
+        setMinimumSize(frameD);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(true);
+
+        jPanel2.setSize(450, 450);
+        jPanel2.setVisible(true);
+        jPanel2.setBorder(new ButtonBorder(Color.black, Color.darkGray, Color.lightGray, Color.lightGray));
+
+        jPanel3.setSize(450, 100);
+        Button b = new Button("restart");
+        b.addActionListener(this);
+        jPanel3.add(b);
+        jPanel3.setVisible(true);
+        initPanel();
+        
+        jPanel2.add(jPanel3);
+        jPanel2.add(jPanel1);
+        this.add(jPanel2);
+        jPanel1.setVisible(true);
+        setVisible(true);
+    }
+    
+    void initPanel(){
+        jPanel1.setMinimumSize(PanalD);
+        jPanel1.setBorder(new ButtonBorder(Color.black, Color.darkGray, Color.lightGray, Color.lightGray));
+
+        GridLayout gl = new GridLayout(8, 8);
+        jPanel1.setLayout(gl);
+        ButtonBorder br = new BasicBorders.ButtonBorder(Color.black, Color.darkGray, Color.lightGray, Color.lightGray);
+        Icon icn =new ImageIcon("tile.png");
+        Font f=new Font("Serif", Font.BOLD, 15);
+        
+        for (int i = 0; i < 8; i++) {
+            System.out.println("minesweepergame.MinesweeperGame.initComponents()");
+            for (int j = 0; j < 8; j++) {
+                list[i][j] = new JButton(i + " " + j,icn);
+                list[i][j].setVisible(true);
+                list[i][j].setFont(f);
+                list[i][j].addActionListener(this);
+                jPanel1.add(list[i][j]); //adds button to grid
+            }
+        }
+    }
+    static MinesweeperGame mine;
+    public static void main(String args[]) {
+        mine = new MinesweeperGame();
+        mine.initComponents();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("restart")) {
+            this.dispose();
+            mine= new MinesweeperGame();
+            mine.initComponents();
+
+        } else {
+            int i = e.getActionCommand().charAt(0) - 48;
+            int j = e.getActionCommand().charAt(2) - 48;
+            int ans = msi.clicked(i, j);
+
+            switch (ans) {
+                case 9:
+                    list[i][j].setText("X");
+                    list[i][j].setBackground(Color.red);
+                    msi.gameOver(list);
+                    break;
+                case 0:
+                    msi.openZeros(list, i, j);
+                    break;
+                default:
+                    list[i][j].setText(Integer.toString(ans));
+                    list[i][j].setEnabled(false);
+                    list[i][j].setBackground(Color.yellow);
+                    break;
+            }
+        }
+    }
+}
